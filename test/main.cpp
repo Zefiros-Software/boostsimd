7eef1cb15e39ce249f6981d51d55bf07a16f30d4
PaulVisscher <paul.pev@hotmail.com>
2017-01-23 17:44:50 +0100

    Fix build

--- test/main.cpp
+++ test/main.cpp
@@ -5,8 +5,8 @@ namespace bs = boost::simd;
 
 int main()
 {
-   bs::pack<float,4> p{1.f,2.f,3.f,4.f};
-   std::cout << p + 10*p << "\n";
+    bs::pack<float, 4> p{1.f, 2.f, 3.f, 4.f};
+    std::cout << p + 10 * p << "\n";
 
-   return 0;
+    return 0;
 }
